4d4c318d6c8cd567ad1b05cef1a6ab32b72e0a5a
Rishit <rishpandey8097@gmail.com>
2024-04-25 13:45:36 +0530

    fix: refactor

---
+++ corne/firmware/README.md
@@ -0,0 +1,3 @@
+# Build firmware
+
+- setup qmk
---
+++ corne/firmware/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+#define TAPPING_FORCE_HOLD
+#define TAPPING_TERM 100
+
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
